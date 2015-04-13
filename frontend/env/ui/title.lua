3a76625217e6b38351beca46cf2f65965b6c1231
Administrator <admin@liquidfeedback>
2015-04-13 02:25:02 +0200

    adding ul tag

--- frontend/env/ui/title.lua
+++ frontend/env/ui/title.lua
@@ -3,29 +3,20 @@ function ui.title ( content )
   slot.select ( "title", function ()
     
     -- home link
-    ui.link {
-      module = "index", view = "index",
-      attr = { class = "home", title = _"Home" },
-      content = function ()
-        ui.image { 
-          attr = { class = "icon24", alt = title },
-          static = "icons/48/home.png"
-        }
-      end
-    }
+    ui.tag { tag = 'ul', content = function()
+            ui.tag { tag = 'li', attr = { class = 'home' }, content = function() 
+                    ui.link {
+                      module = "index", view = "index",
+                      content = function() 
+                        ui.i { attr = { class = 'fa fa-home' } }
+                      end
+                    }
+            end}
+
+            -- additional lis
+            ui.tag{ content = content }
+    end }
   
-    if content then
-      ui.tag { attr = { class = "spacer" }, content = function()
-        slot.put ( " » " )
-      end }
-      ui.tag { tag = "span", content = content }
-    else
-      ui.tag { attr = { class = "spacer" }, content = function()
-        slot.put ( " " )
-      end }
-      ui.tag { tag = "span", content = _"Home" }
-    end
-    
   end )
   
 end
