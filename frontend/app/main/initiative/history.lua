311f71fc615aa6195bee97682fa795f807bc83bb
Administrator <admin@liquidfeedback>
2015-04-13 02:31:18 +0200

    updating markup

--- frontend/app/main/initiative/history.lua
+++ frontend/app/main/initiative/history.lua
@@ -34,84 +34,86 @@ execute.view {
     issue = initiative.issue
   }
 }
-
-ui.form{
-  method = "get",
-  module = "draft",
-  view = "diff",
-  attr = { class = "section" },
-  content = function()
-    ui.field.hidden{ name = "initiative_id", value = initiative.id }
-  
-    ui.sectionHead( function()
-      ui.link{
-        module = "initiative", view = "show", id = initiative.id,
-        content = function ()
-          ui.heading { 
-            level = 1,
-            content = initiative.display_name
-          }
-        end
-      }
-      ui.heading { level = 2, content = _"Draft history" }
-    end)
-    
-    ui.sectionRow( function()
-    
-      local columns = {
-        {
-          label = _"draft ID",
-          content = function(record)
-            ui.tag { content = record.id }
-          end
-        },
-        {
-          label = _"published at",
-          content = function(record)
-            ui.link{
-              attr = { class = "action" },
-              module = "draft", view = "show", id = record.id,
-              text = format.timestamp(record.created)
-            }
-          end
-        },
-        {
-          label = _"compare",
-          content = function(record)
-            slot.put('<input type="radio" name="old_draft_id" value="' .. tostring(record.id) .. '">')
-            slot.put('<input type="radio" name="new_draft_id" value="' .. tostring(record.id) .. '">')
+ui.container{ attr = { class='section section-drafthistory' }, content = function()
+        ui.form{
+          method = "get",
+          module = "draft",
+          view = "diff",
+          attr = { class = "section" },
+          content = function()
+            ui.field.hidden{ name = "initiative_id", value = initiative.id }
+          
+            ui.sectionHead( function()
+              ui.link{
+                module = "initiative", view = "show", id = initiative.id,
+                content = function ()
+                  ui.heading { 
+                    level = 1,
+                    content = initiative.display_name
+                  }
+                end
+              }
+              ui.heading { level = 2, content = _"Draft history" }
+            end)
+            
+            ui.sectionRow( function()
+            
+              local columns = {
+                {
+                  label = _"draft ID",
+                  content = function(record)
+                    ui.tag { content = record.id }
+                  end
+                },
+                {
+                  label = _"published at",
+                  content = function(record)
+                    ui.link{
+                      attr = { class = "action" },
+                      module = "draft", view = "show", id = record.id,
+                      text = format.timestamp(record.created)
+                    }
+                  end
+                },
+                {
+                  label = _"compare",
+                  content = function(record)
+                    slot.put('<input type="radio" name="old_draft_id" value="' .. tostring(record.id) .. '">')
+                    slot.put('<input type="radio" name="new_draft_id" value="' .. tostring(record.id) .. '">')
+                  end
+                }
+              }
+              
+              if app.session:has_access("authors_pseudonymous") then
+                columns[#columns+1] = {
+                  label = _"author",
+                  content = function(record)
+                    if record.author then
+                      return util.micro_avatar ( record.author )
+                    end
+                  end
+                }
+              end
+              
+              ui.list{
+                records = initiative.drafts,
+                columns = columns
+              }
+              
+              slot.put("<br />")
+              ui.container { attr = { class = "actions" }, content = function()
+                ui.tag{
+                  tag = "input",
+                  attr = {
+                    type = "submit",
+                    class = "btn btn-default",
+                    value = _"compare revisions"
+                  },
+                  content = ""
+                }
+              end }
+            end )
           end
         }
-      }
-      
-      if app.session:has_access("authors_pseudonymous") then
-        columns[#columns+1] = {
-          label = _"author",
-          content = function(record)
-            if record.author then
-              return util.micro_avatar ( record.author )
-            end
-          end
-        }
-      end
-      
-      ui.list{
-        records = initiative.drafts,
-        columns = columns
-      }
-      
-      slot.put("<br />")
-      ui.container { attr = { class = "actions" }, content = function()
-        ui.tag{
-          tag = "input",
-          attr = {
-            type = "submit",
-            class = "btn btn-default",
-            value = _"compare revisions"
-          },
-          content = ""
-        }
-      end }
-    end )
-  end
+end
 }
