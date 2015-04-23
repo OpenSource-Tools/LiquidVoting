31ebb4a5de24701dc5aa7b42b2fe9c1c3da819cf
Administrator <admin@liquidfeedback>
2015-04-23 17:23:23 +0200

    classes

--- frontend/app/main/initiative/_sidebar_policies.lua
+++ frontend/app/main/initiative/_sidebar_policies.lua
@@ -1,8 +1,11 @@
 local area = param.get("area", "table")
 
-ui.sidebar ( "tab-whatcanido", function ()
+ui.sidebar ( "tab tab-initiative-policies", function ()
   ui.sidebarHead( function()
-    ui.heading { level = 2, content = _"Available policies" }
+    ui.heading { 
+
+            attr = { class = 'h2' },
+            level = 2, content = _"Available policies" }
   end )
   execute.view { module = "policy", view = "_list", params = {
     for_area = area
