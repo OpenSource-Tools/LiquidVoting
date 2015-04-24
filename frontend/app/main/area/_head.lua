9bfa7ffe9c0e8908f964a6ee7891530ee410d57c
Administrator <admin@liquidfeedback>
2015-04-24 22:53:36 +0200

    adding subscribed class to area breadcrumb

--- frontend/app/main/area/_head.lua
+++ frontend/app/main/area/_head.lua
@@ -1,6 +1,12 @@
 local area = param.get("area", "table")
 local member = param.get("member", "table")
 
+
+local subscribed = area.delegation_info.own_participation
+local class = 'area'
+if subscribed then class = class .. ' subscribed'
+end
+
 ui.title ( function ()
 
   ui.tag { tag = 'li', attr = { class = 'unit' }, content = function()
@@ -16,7 +22,7 @@ ui.title ( function ()
     }
   end }
 
-  ui.tag { tag = 'li', attr = { class = 'area' }, content = function()
+  ui.tag { tag = 'li', attr = { class = class }, content = function()
       ui.link {
         content = function()
           ui.tag{ content = function()
@@ -33,5 +39,5 @@ ui.title ( function ()
         }
       }
   end }
-  
+  ;
 end )
